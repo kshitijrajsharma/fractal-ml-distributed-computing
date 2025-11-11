95387abff959d23488c58e257ca184ca0aa520b3
kshitijrajsharma <skshitizraj@gmail.com>
2025-11-11 19:39:25 +0100

    result : add more results on x4 with updated profile to run both on local and server

--- scripts/Readme.md
+++ scripts/Readme.md
@@ -4,13 +4,13 @@ Server :
 
 ```bash
 
-spark-submit --deploy-mode cluster --master yarn train.py --data "s3a://ubs-datasets/FRACTAL/data" --executor-cores 2 --num-executors 16 --executor-memory  8--driver-memory 8 --fraction 0.01
+spark-submit --deploy-mode cluster --master yarn train.py --data "s3a://ubs-datasets/FRACTAL/data" --executor-cores 2 --num-executors 16 --executor-memory 8 --driver-memory 2 --fraction 0.01
 ```
 
 Local :
 
 ```bash
-sudo docker exec spark-master spark-submit scripts/train.py  --master spark://spark-master:7077 --data "/opt/spark/work-dir/data/FRACTAL" --executor-cores 3 --num-executors 4 --executor-memory 7 --driver-memory 8 --fraction 0.0000001 --enable-stage-metrics --output "/opt/spark/work-dir/results"
+sudo docker exec spark-master spark-submit scripts/train.py  --master spark://spark-master:7077 --data "/opt/spark/work-dir/data/FRACTAL" --executor-cores 2 --num-executors 2 --executor-memory 7 --driver-memory 1 --fraction 0.01 --enable-stage-metrics --output "/opt/spark/work-dir/results"
 ```
 
 Use Profile :
@@ -25,4 +25,10 @@ execute
 
 ```bash
 sudo bash profile.sh
+```
+
+To run the profile in the cluster
+
+```bash
+sudo bash profile.sh server
 ```
--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+ORG=${1:-local}
 NUM_EXECUTORS_LIST=(1 2 4)
 DATA_FRACTIONS=(0.01 0.05 0.1)
 
-for num_exec in "${NUM_EXECUTORS_LIST[@]}"; do
-    for frac in "${DATA_FRACTIONS[@]}"; do
-        echo "Running config: executors=${num_exec}, fraction=${frac}"
-        sudo docker exec spark-master spark-submit scripts/train.py --master spark://spark-master:7077 --data /opt/spark/work-dir/data/FRACTAL  --num-executors ${num_exec} --fraction ${frac} --executor-cores 2  --executor-memory 7 --driver-memory 1 --enable-stage-metrics --output /opt/spark/work-dir/results
-        echo "Completed config: executors=${num_exec}, fraction=${frac}"
-        echo "---"
+if [ "$ORG" = "local" ]; then
+    echo "Running in LOCAL mode"
+    for num_exec in "${NUM_EXECUTORS_LIST[@]}"; do
+        for frac in "${DATA_FRACTIONS[@]}"; do
+            echo "Running config: executors=${num_exec}, fraction=${frac}"
+            sudo docker exec spark-master spark-submit scripts/train.py --master spark://spark-master:7077 --data /opt/spark/work-dir/data/FRACTAL --num-executors ${num_exec} --fraction ${frac} --executor-cores 2 --executor-memory 7 --driver-memory 1 --enable-stage-metrics --output /opt/spark/work-dir/results
+            echo "Completed config: executors=${num_exec}, fraction=${frac}"
+            echo "---"
+        done
     done
-done
+else
+    echo "Running in SERVER mode"
+    for num_exec in "${NUM_EXECUTORS_LIST[@]}"; do
+        for frac in "${DATA_FRACTIONS[@]}"; do
+            echo "Running config: executors=${num_exec}, fraction=${frac}"
+            spark-submit --deploy-mode cluster --master yarn scripts/train.py --data "s3a://ubs-datasets/FRACTAL/data" --num-executors ${num_exec} --fraction ${frac} --executor-cores 2 --executor-memory 7 --driver-memory 1 --enable-stage-metrics --output /opt/spark/work-dir/results
+            echo "Completed config: executors=${num_exec}, fraction=${frac}"
+            echo "---"
+        done
+    done
+fi
 
 echo "All profiling runs completed!"
